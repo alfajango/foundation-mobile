548d0a3b4374f78c446147e865c344cb3b5d5732
Steve Schwartz <steve@alfajango.com>
2013-06-26 01:24:26 -0400

    Added full-size dropdowns to docs.

--- docs/js/foundation-mobile.js
+++ docs/js/foundation-mobile.js
@@ -9,23 +9,30 @@
 ;(function ($, window, document, undefined) {
   'use strict';
 
+  Foundation.libs.dropdown.full_size = function(dropdown) {
+      var $window = $(window);
+
+      // If bottom of dropdown is above bottom of window
+      if (dropdown.hasClass('full-size') && (dropdown.offset().top + dropdown.outerHeight()) < ($window.scrollTop() + $window.height())) {
+          dropdown.height($window.height() - (dropdown.offset().top - $window.scrollTop()) - 10)
+      }
+  };
+
   Foundation.libs.dropdown.resize = function () {
       var dropdown = $('[data-dropdown-content].open'),
           target = $("[data-dropdown='" + dropdown.attr('id') + "']");
 
       if (dropdown.length && target.length) {
         this.css(dropdown, target);
-        this.mobile_css(dropdown, target);
+        this.full_size(dropdown);
       }
   };
 
-  Foundation.libs.dropdown.mobile_css = function(dropdown, target) {
-      var $window = $(window);
+  $(document).foundation('dropdown', { opened: function(e) {
+      var $window = $(window),
+          dropdown = $(this);
 
-      // If bottom of dropdown is above bottom of window
-      if ((target.position().top + target.outerHeight()) < ($window.scrollTop() + $window.height())) {
-          target.height($window.height() - (target.offset().top - $window.scrollTop()) - 10)
-      }
-  };
+      Foundation.libs.dropdown.full_size(dropdown);
+  }});
 
 }(Foundation.zj, this, this.document));
