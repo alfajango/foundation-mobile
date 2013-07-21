f815e92369753de4bfda6c81a1b631a029dc38ad
Steve Schwartz <steve@alfajango.com>
2013-07-21 10:47:56 -0400

    Compiled latest stylesheets and javascripts.

--- docs/js/foundation-mobile.js
+++ docs/js/foundation-mobile.js
@@ -18,7 +18,7 @@
   Foundation.libs.bottombar = {
     name : 'bottombar',
 
-    version : '4.2.3',
+    version : '4.1.5',
 
     settings : {
       init : false
@@ -206,7 +206,191 @@
     reflow : function () {}
   };
 }(Foundation.zj, this, this.document));
+/*jslint unparam: true, browser: true, indent: 2 */
+
+
+;(function ($, window, document, undefined) {
+  'use strict';
+
+  Foundation.libs.breadcrumbs = {
+    name : 'breadcrumbs',
+
+    version : '4.1.5',
+
+    settings : {
+      init : false
+    },
+
+    init : function (section, method, options) {
+      Foundation.inherit(this, 'data_options');
+      var self = this;
+
+      if (typeof method === 'object') {
+        $.extend(true, this.settings, method);
+      } else if (typeof options !== 'undefined') {
+        $.extend(true, this.settings, options);
+      }
+
+      if (typeof method !== 'string') {
+        // Breadcrumbs init code here
+
+        this.events();
+
+        return this.settings.init;
+      } else {
+        // fire method
+        return this[method].call(this, options);
+      }
+    },
+
+    events : function () {
+      var self = this;
+
+      $(this.scope)
+        .off('.fndtn.breadcrumbs')
+        .on('click.fndtn.breadcrumbs', '.mobile-breadcrumbs', function (e) {
+          e.preventDefault();
+
+          var element = $(this);
+        });
+
+      return true;
+    },
+
+    reflow : function () {}
+  };
+}(Foundation.zj, this, this.document));
+/*jslint unparam: true, browser: true, indent: 2 */
+
+
+;(function ($, window, document, undefined) {
+  'use strict';
+
+  Foundation.libs.collapsible = {
+    name : 'collapsible',
+
+    version : '4.1.5',
+
+    settings : {
+      init : false
+    },
+
+    init : function (scope, method, options) {
+      var self = this;
+      this.scope = scope || this.scope;
+
+      if (typeof method === 'object') {
+        $.extend(true, this.settings, method);
+      } else if (typeof options !== 'undefined') {
+        $.extend(true, this.settings, options);
+      }
+
+      if (typeof method !== 'string') {
+        $('[data-collapsible]').each(function() {
+          var $collapsible = $(this);
+
+          if($collapsible.is('[data-collapsed]')) {
+            self.collapse($collapsible);
+          }
+        });
+
+        self.events();
+
+        return this.settings.init;
+      } else {
+        // fire method
+        return this[method].call(this, options);
+      }
+    },
+
+    events : function () {
+      var self = this;
+
+      $(this.scope)
+        .off('.fndtn.collapsible')
+        .on('click.fndtn.collapsible', '[data-collapsible]', function (e) {
+          var element = $(this).closest('[data-collapsible]');
+          if (element.hasClass('collapsed')) {
+            self.uncollapse(element);
+          } else {
+            self.collapse(element);
+          }
+          e.preventDefault();
+        });
+
+      return true;
+    },
+
+    collapse: function(element) {
+      var self = this;
+      element.addClass('collapsed');
+      element.children().not(':first-child').hide();
+    },
+
+    uncollapse: function(element) {
+      var self = this;
+      element.children().show();
+      element.removeClass('collapsed');
+    },
+
+    reflow : function () {}
+  };
+}(Foundation.zj, this, this.document));
+/*jslint unparam: true, browser: true, indent: 2 */
+
+
+;(function ($, window, document, undefined) {
+  'use strict';
+
+  Foundation.libs.back = {
+    name : 'back',
+
+    version : '4.1.5',
+
+    settings : {
+      init : false
+    },
+
+    init : function (scope, method, options) {
+      var self = this;
+      this.scope = scope || this.scope;
+
+      if (typeof method === 'object') {
+        $.extend(true, this.settings, method);
+      } else if (typeof options !== 'undefined') {
+        $.extend(true, this.settings, options);
+      }
+
+      if (typeof method !== 'string') {
+        self.events();
+
+        return this.settings.init;
+      } else {
+        // fire method
+        return this[method].call(this, options);
+      }
+    },
+
+    events : function () {
+      var self = this;
+
+      $(this.scope)
+        .off('.fndtn.back')
+        .on('click.fndtn.back', '[data-back]', function (e) {
+          window.history.go(-1);
+          e.preventDefault();
+        });
+
+      return true;
+    },
+
+    reflow : function () {}
+  };
+}(Foundation.zj, this, this.document));
 /*
+
+
+
 
 
 
